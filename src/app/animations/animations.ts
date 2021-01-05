3ec4f0c3a21de1242fb84612162b7d1ca4466f11
ecorrea95 <e.correa95@hotmail.com>
2021-01-04 21:56:51 -0300

    Updated Logo Navbar

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -75,8 +75,8 @@ import {
                    query(':leave', [
                         style({ transform: 'translateX(0%)' }),
                         animate('0.5s ease-in-out', 
-                        style({ transform: 'translateX(100%)' })
-                   ], { optional: true }),
+                        style({ transform: 'translateX(100%)' }),
+                    ], { optional: true }),
                ]),
         ]),
         transition('About => Projects', [
@@ -130,4 +130,22 @@ import {
             ], { optional: true }),
         ]),
     ]),
+    transition('Projects => Home', [
+        query(':enter, :leave', 
+            style({ position: 'fixed', width: '100%' }), 
+            { optional: true }),
+        group([
+            query(':enter', [
+                style({ transform: 'translateX(-100%)' }),
+                animate('0.5s ease-in-out', 
+                style({ transform: 'translateX(0%)' }))
+            ], { optional: true }),
+            query(':leave', [
+                 style({ transform: 'translateX(0%)' }),
+                 animate('0.5s ease-in-out', 
+                 style({ transform: 'translateX(100%)' }),
+            ], { optional: true }),
+        ]),
+    ]),
+
  ]);
